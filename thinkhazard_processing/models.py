11691ec5145339befb324257665a93d551e962f3
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2015-11-19 17:32:51 +0100

    Use yaml for settings

--- thinkhazard_processing/models.py
+++ thinkhazard_processing/models.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import os
 from sqlalchemy import (Column, ForeignKey,
                         Boolean, Date, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
