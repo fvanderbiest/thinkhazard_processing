8dc6e78bc8076428285f4d9874f28597fe949dc2
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2015-12-04 11:55:47 +0100

    Try to fix transaction in travis tests

--- thinkhazard_processing/scripts/initializedb.py
+++ thinkhazard_processing/scripts/initializedb.py
@@ -4,22 +4,19 @@ from sqlalchemy import engine_from_config
 from .. import models  # NOQA
 from .. import settings
 
-from thinkhazard_common.scripts.initializedb import initdb, schema_exists
+from thinkhazard_common.scripts.initializedb import (
+    initdb,
+    schema_exists,
+    )
 
 
 def initdb_processing(engine, drop_all=False):
-    connection = engine.connect()
-    trans = connection.begin()
-    try:
-        if not schema_exists(connection, 'processing'):
-            connection.execute("CREATE SCHEMA processing;")
-        initdb(connection, drop_all=drop_all)
-        trans.commit()
-    except:
-        trans.rollback()
-        raise
+    if not schema_exists(engine, 'processing'):
+        engine.execute("CREATE SCHEMA processing;")
+    initdb(engine, drop_all=drop_all)
 
 
 def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
-    initdb_processing(engine)
+    with engine.begin() as db:
+        initdb_processing(db)
--- thinkhazard_processing/tests/__init__.py
+++ thinkhazard_processing/tests/__init__.py
@@ -1,8 +1,8 @@
 import yaml
 from sqlalchemy import engine_from_config
-import transaction
 
 from .. import settings
+from ..scripts.initializedb import initdb_processing
 from .test_data import populate
 
 
@@ -15,11 +15,7 @@ with open(local_settings_path, 'r') as f:
 
 def populate_db():
     engine = engine_from_config(settings, 'sqlalchemy.')
-
-    from ..scripts.initializedb import initdb_processing
     initdb_processing(engine, True)
-
-    with transaction.manager:
-        populate()
+    populate()
 
 populate_db()
