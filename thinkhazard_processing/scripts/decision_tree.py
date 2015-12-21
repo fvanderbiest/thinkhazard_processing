8ef9bc0ca29af0e2aec5b74395178e176b85f401
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2015-12-21 11:16:24 +0100

    fixed a typo

--- thinkhazard_processing/scripts/decision_tree.py
+++ thinkhazard_processing/scripts/decision_tree.py
@@ -13,7 +13,7 @@ def main(argv=sys.argv):
     parser.add_argument(
         '--dry-run', dest='dry_run',
         action='store_const', const=True, default=False,
-        help='Perform a trial run that do not commit changes')
+        help='Perform a trial run that does not commit changes')
     args = parser.parse_args(argv[1:])
 
     engine = engine_from_config(settings, 'sqlalchemy.')
